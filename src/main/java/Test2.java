dc11341ab0a7f4190ca5840b4ce6d294720c6c41
ZhangBingcheng <43721575+ZhangBingcheng@users.noreply.github.com>
2019-01-10 21:29:24 +0800

    第一次提交

---
+++ src/main/java/Test2.java
@@ -0,0 +1,8 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println ("111111");
+        System.out.println ("222222");
+
+        System.out.println ("1232");
+    }
+}
